a3f67fe24becc133689634dd50c512ffefc6be01
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2018-02-22 17:43:51 +1100

    react redux search functioanlity

---
+++ src/features/common/app-header/app-header.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import {Link} from 'react-router-dom';
+import {CustomerSearch} from "../../../features";
+import {ReButton,ReMenu,ReMenuItem} from "../../common";
+import {Button} from 'react-bootstrap';
+import './app-header.scss';
+
+export default function HeaderApp() {
+	return ( 
+			<div id = "sidebar-wrapper" >
+				<ul className = "sidebar-nav" >
+					<li>
+						<Link to = "/" > Home < /Link>
+					</li> 
+					<li>
+						<Link to = "/search" > Search Cutomers < /Link>
+					</li>
+				</ul>
+			</div>
+    )
+
+}
